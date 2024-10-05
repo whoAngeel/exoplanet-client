1b68bde7d1fb8b7025c29f93d704ea1c982cb88b
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-10-05 14:54:18 -0600

    Limpiando el proyecto

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,15 +5,6 @@ import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-d
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Game from "../pages/game/Game";
-import GameLayout from "../pages/game/GameLayout";
-import ProtectedRoute from "../utils/ProtectedRoute";
-import Logout from "../pages/Logout";
-import LayoutFunding from "../pages/crowdfunding/Layout.jsx";
-import IndexCrowdFunding from "../pages/crowdfunding/Index";
-import AllFunding from "../pages/crowdfunding/All";
-import StartNowFunding from "../pages/crowdfunding/StartNow.jsx";
-import Trivia from "../pages/trivia/Trivia.jsx";
 
 const router = createBrowserRouter([
 	{
@@ -28,48 +19,7 @@ const router = createBrowserRouter([
 		path: "/register",
 		element: <Register />,
 	},
-	{
-		path: "/crowdfunding",
-		element: <LayoutFunding />,
-		children: [
-			{
-				index: true,
-				element: <IndexCrowdFunding />,
-			},
-			{
-				path: "all",
-				element: <AllFunding />,
-			},
-			{
-				path: "startnow",
-				element: <StartNowFunding />,
-			},
-		],
-	},
-	{
-		path: "/trivia",
-		element: <Trivia />,
-		children: [
-			{
-				index: true,
-				element: <Trivia />,
-			},
-		],
-	},
-	{
-		path: "/game",
-		element: (
-			<ProtectedRoute>
-				<GameLayout />
-			</ProtectedRoute>
-		),
-		children: [
-			{
-				index: true,
-				element: <Game />,
-			},
-		],
-	},
+
 	{
 		path: "/logout",
 		element: <Logout />,
