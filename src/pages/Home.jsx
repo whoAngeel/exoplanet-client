2a4139970eedf1fc0c6cc2e7039241d4c02fe4e2
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-10-06 01:20:21 -0600

    models 3d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import EarthCartoon from "../../public/earthCarton/EarthCartoon";
+import Earth from "../../public/earth/Earth";
+import Pegasi from "../../public/51PegasiB/Pegasi";
+import Kepler452b from "../../public/kepler452b/Kepler452b"
+import Corot7bfiery from "../../public/corot7bfiery/Corot7bfiery"
+import Kepler186f from "../../public/kepler186f/Kepler186f"
+import Kepler22b from "../../public/kepler22b/Kepler22b"
+import Proxima from "../../public/proxima/Proxima"
 import { ARButton, XR } from "@react-three/xr";
 
 function Home() {
@@ -13,11 +19,28 @@ function Home() {
           <ambientLight />
           <OrbitControls />
           <Suspense fallback={null}>
-            <mesh scale={[0.5, 0.5, 0.5]}
-			position={[0,0,-3]}>
-
-            <EarthCartoon />
-			</mesh>
+            <mesh>
+              <Pegasi />
+            </mesh>
+            <mesh
+            position={[3,0,0]}>
+              <Earth />
+            </mesh>
+            <mesh
+            position={[-3,0,0]} 
+            scale={[0.01, 0.01, 0.01]}>
+              <Kepler452b/>
+            </mesh>
+            <mesh
+            position={[6,0,0]} 
+            scale={[0.001, 0.001, 0.001]}>
+              <Corot7bfiery/>
+            </mesh>
+            <mesh
+            position={[-7,0,0]} 
+            scale={[0.3, 0.3, 0.3]}>
+              <Kepler22b/>
+            </mesh>
           </Suspense>
         </XR>
       </Canvas>
