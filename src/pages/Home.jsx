27fce5f61aea1d3458bdbcbc58061fd64b5a6f7a
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-10-05 18:00:11 -0600

    First Planet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import EarthCartoon from "../../public/earthCarton/EarthCartoon"
-
+import EarthCartoon from "../../public/earthCarton/EarthCartoon";
+import { ARButton, XR } from "@react-three/xr";
 
 function Home() {
-	
-	return (
-		<>
-		<Canvas>
-			<ambientLight/>
-			<OrbitControls/>
-			<Suspense fallback={null}>
-				<EarthCartoon/>
-			</Suspense>
-		</Canvas>
-		</>
-	);
+  return (
+    <>
+      <ARButton />
+      <Canvas>
+        <XR>
+          <ambientLight />
+          <OrbitControls />
+          <Suspense fallback={null}>
+            <mesh scale={[0.5, 0.5, 0.5]}
+			position={[0,0,-3]}>
+
+            <EarthCartoon />
+			</mesh>
+          </Suspense>
+        </XR>
+      </Canvas>
+    </>
+  );
 }
 
 export default Home;
